50f3d903fd72452bef917ecbf2ca04e12ffe0357
mobo <morgan.bohn@pc-scol.fr>
2023-01-31 14:27:55 +0100

    [TECH] add NgrxDateTimeZoneToISO8601

--- projects/ngrx-forms-material/src/lib/converters/dateonly.converter.ts
+++ projects/ngrx-forms-material/src/lib/converters/dateonly.converter.ts
@@ -1,5 +1,5 @@
 import { NgrxValueConverter, NgrxValueConverters } from 'ngrx-forms';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 /**
  * Converter for dates in ISO8601 format for ngrx-forms
@@ -40,5 +40,25 @@ export class NgrxDateToISO8601UTC
   convertStateToViewValue = (s) => {
     const date = NgrxValueConverters.dateToISOString.convertStateToViewValue(s);
     return date !== null ? moment.utc(date) : null;
+  };
+}
+
+/**
+ * Converter for dates with time and timezone in ISO8601 format for ngrx-forms
+ */
+export class NgrxDateTimeZoneToISO8601 implements NgrxValueConverter<moment.Moment | null, string | null> {
+  constructor(private tz: string) {}
+
+  /**
+   * Transform the view into state for the store
+   */
+  convertViewToStateValue = (date: moment.Moment) => (date === null ? null : date.tz(this.tz, true).format());
+
+  /**
+   * Transform the state in the store into the view
+   */
+  convertStateToViewValue = (s) => {
+      const date = NgrxValueConverters.dateToISOString.convertStateToViewValue(s);
+      return date !== null ? moment.tz(date, this.tz) : null;
   };
 }
